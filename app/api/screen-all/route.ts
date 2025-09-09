69f96d538277d1ff7913769b0e909846006ddc7e
Rahul Gupta <rg5353070@gmail.com>
2025-09-09 20:23:55 +0530

    changed the route logic which now includes sending topics to google cloud pub sub

--- app/api/screen-all/route.ts
+++ app/api/screen-all/route.ts
@@ -1,6 +1,9 @@
 import { auth } from "@/auth";
+import { pubSubClient } from "@/lib/pubsub-client";
 import { redisClient } from "@/lib/redis";
 import { NextResponse } from "next/server";
+
+const topicName = process.env.PUBSUB_TOPIC_NAME;
 
 export async function POST(request: Request) {
   const userSession = await auth();
@@ -39,6 +42,9 @@ export async function POST(request: Request) {
   }
 
   try {
+    // We'll collect all the publish promises to run them in parallel
+    const publishPromises: Promise<string>[] = [];
+
     // Enqueue each deal (await to ensure completion before publish)
     for (const dealListing of dealListings) {
       const payload = {
@@ -48,16 +54,29 @@ export async function POST(request: Request) {
         screenerContent,
         screenerName,
       };
-      await redisClient.rpush("dealListings", JSON.stringify(payload));
+      // await redisClient.rpush("dealListings", JSON.stringify(payload));
+
+      const dataBuffer = Buffer.from(JSON.stringify(payload));
+      const publishPromise = pubSubClient
+        .topic(topicName!)
+        .publishMessage({ data: dataBuffer });
+
+      publishPromises.push(publishPromise);
     }
 
     // Notify via pub/sub that new items are available for this user
-    await redisClient.publish(
-      "new_screen_call",
-      JSON.stringify({ userId: userSession.user.id }),
-    );
+    // await redisClient.publish(
+    //   "new_screen_call",
+    //   JSON.stringify({ userId: userSession.user.id }),
+    // );
 
-    return NextResponse.json({ message: "Enqueued" });
+    // Await all messages to be published
+    console.log("before promise.all");
+    await Promise.all(publishPromises);
+
+    console.log("all promises ran successfully");
+
+    return NextResponse.json({ message: "Jobs Published Successfully" });
   } catch (error) {
     console.error("Error enqueuing deals:", error);
     return NextResponse.json(
