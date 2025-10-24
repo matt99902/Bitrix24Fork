2ad3e97df4cbbf212691f3bd24ec9dbcd5697e59
Rahulg321 <rg5353070@gmail.com>
2025-10-24 17:22:14 +0530

    Refactor Prisma client initialization for optimized performance
    - Switched from the standard PrismaClient to the edge version for improved efficiency.
    - Integrated the withAccelerate extension to enhance database interactions.
    - Removed global singleton pattern for a more straightforward client instantiation.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: ["info"],
-  });
-};
-
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prismaDB = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prismaDB = new PrismaClient().$extends(withAccelerate());
 
 export default prismaDB;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prismaDB;
